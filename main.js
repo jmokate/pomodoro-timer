d9094e7aa8d2741c3992d9ec177abfec871c2822
jmokate <jmokate@gmail.com>
2019-11-06 10:08:34 -0600

    added work and break buttons that are functional

--- main.js
+++ main.js
@@ -1,57 +1,87 @@
+const workBtn = document.getElementById('work');
+const breakBtn = document.getElementById('break');
 const startStopBtn = document.getElementById('startStop');
 const resetTime = document.getElementById('resetBtn');
-
+const displayModeText = document.getElementById('heading-2')
 const defaultNum = '0' + '0';
 let min = 0;
 let sec = 5;
 
 
-let minText = document.getElementById('minutes');
-minText.innerText = min;
+let currentMinEl = document.getElementById('minutes');
+currentMinEl.innerText = min;
 
-let secondsText = document.getElementById('seconds');
-secondsText.innerText = defaultNum;
+let currentSecEl = document.getElementById('seconds');
+currentSecEl.innerText = defaultNum;
 
-
-let isActive = false;
 let currentIntervalId;
+let isActive = false;
 let isBreak = false;
-//let isWork = true;
 
 //EVENT LISTENERS
+
+workBtn.addEventListener('click', function() {
+  min = 0;
+  sec = 5;
+  displayModeText.innerText = 'Hit Start to Get Back to Work';
+  displayModeText.style.color = 'red';
+  resetTime.disabled = true;
+});
+breakBtn.addEventListener('click', function(){
+  min = 0;
+  sec = 6;
+  displayModeText.innerText = 'Hit Start to Take A Break';
+  displayModeText.style.color = 'green';
+  resetTime.disabled = true;
+})
+
 
 startStopBtn.addEventListener('click', function() {
   if (isActive == true) {
     //stopCount();
     clearInterval(currentIntervalId);
+    workBtn.disabled = false;
+    breakBtn.disabled = false;
     resetTime.disabled = false;
     startStopBtn.innerText = 'start';
     isActive = !isActive;
+    if (min == 0 && sec == 0) {
+      if (isBreak == false) {
+        displayModeText.innerText = 'Hit Start to Take A Break';
+        displayModeText.style.color = 'green';
+        return;
+      } displayModeText.innerText = 'Hit Start to Get Back to Work';
+        displayModeText.style.color = 'red';
+    }
     return;
   } //else if (isActive === false) {
     currentIntervalId = setInterval(decrement, 1000);
     resetTime.disabled = true;
     isActive = true;
     startStopBtn.innerText = 'stop';
+    workBtn.disabled = true;
+    breakBtn.disabled = true;
     if (min == 0 && sec ==0){
-      resetTime.disabled = false;
+      resetTime.disabled = true;
     }
-
-  console.log(isActive);
+    if (min == 0 && sec == 0) {
+      if (isBreak == false) {
+        isBreak = true; //sets to work time
+        min = 0;
+        sec = 6;
+        //displayModeText.innerText = 'Hit Start to Take A Break';
+        //startStopBtn.disabled = false;
+      } else if (isBreak == true) {
+        isBreak = !isBreak; //sets to break time
+        min = 0;
+        sec = 5;
+        //startStopBtn.disabled = false;
+      }
+    }
+    console.log(isBreak);
+  //console.log(isActive);
 })
 
-// startStopButton.addEventListener('click', function() {
-//   if (isActive === false) {
-//     isActive = true;
-//     resetTime.disabled = true;
-//     startStopButton.innerText = 'stop';
-//     currentIntervalId = setInterval(decrement, 1000);
-//   } else if (isActive === true) {
-//     isActive = !isActive;
-//     stopCount();
-//   }
-//   console.log(isActive);
-// });
 
 resetTime.addEventListener('click', function(e) {
   if (min == 0 && sec == 0) {
@@ -78,32 +108,25 @@ resetTime.addEventListener('click', function(e) {
   console.log(isBreak);
 });
 
-
-
-
 //MAIN TIMER FUNCTION
 function decrement() {
   sec--;
-  secondsText.innerText = sec;
+  currentSecEl.innerText = sec;
   if (sec < 10){
-    secondsText.innerText = '0' + sec;
+    currentSecEl.innerText = '0' + sec;
   }
   if (min < 10) {
-    minText.innerText = '0' + min;
+    currentMinEl.innerText = '0' + min;
   }
   if (sec < 0) {
     min--;
-    minText.innerText = min;
+    currentMinEl.innerText = min;
     sec = 59;
-    secondsText.innerText = sec;
+    currentSecEl.innerText = sec;
   }
   if (sec == 0 && min == 0) {
     clearTimeout(currentIntervalId);    
     //startStopBtn.disabled = true;
     //resetTime.disabled = false;
-  
-   // min = 0;
-   // sec = 6;
-    //startTime.addEventListener('click', breakTimer());
   }
 }
