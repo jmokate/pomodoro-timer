8fb7a878dd7be255de058fef9088459eed83bf2a
jmokate <jmokate@gmail.com>
2019-11-06 18:05:07 -0600

    timer fully functional

--- main.js
+++ main.js
@@ -2,49 +2,49 @@ const workBtn = document.getElementById('work');
 const breakBtn = document.getElementById('break');
 const startStopBtn = document.getElementById('startStop');
 const resetTime = document.getElementById('resetBtn');
-const displayModeText = document.getElementById('heading-2')
+const displayModeText = document.getElementById('heading-2');
+const audioBell = new Audio('bell.wav');
 const defaultNum = '0' + '0';
-let min = 0;
-let sec = 5;
 
-
-let currentMinEl = document.getElementById('minutes');
-currentMinEl.innerText = min;
-
-let currentSecEl = document.getElementById('seconds');
-currentSecEl.innerText = defaultNum;
-
+let min = 25;
+let sec = 60;
 let currentIntervalId;
 let isActive = false;
 let isBreak = false;
 
+const currentMinEl = document.getElementById('minutes');
+currentMinEl.innerText = min;
+const currentSecEl = document.getElementById('seconds');
+currentSecEl.innerText = defaultNum;
+
 //EVENT LISTENERS
 
 workBtn.addEventListener('click', function() {
-  min = 0;
-  sec = 5;
+  isBreak = false;
+  workTimeDisplay();
   displayModeText.innerText = 'Hit Start to Get Back to Work';
   displayModeText.style.color = 'red';
   resetTime.disabled = true;
 });
 breakBtn.addEventListener('click', function(){
-  min = 0;
-  sec = 6;
+  isBreak = true;
+  breakTimeDisplay();
   displayModeText.innerText = 'Hit Start to Take A Break';
   displayModeText.style.color = 'green';
   resetTime.disabled = true;
+  console.log(isBreak);
 })
 
 
 startStopBtn.addEventListener('click', function() {
   if (isActive == true) {
-    //stopCount();
     clearInterval(currentIntervalId);
     workBtn.disabled = false;
     breakBtn.disabled = false;
     resetTime.disabled = false;
     startStopBtn.innerText = 'start';
     isActive = !isActive;
+    //return; was here but greyed out below (and below code still worked)
     if (min == 0 && sec == 0) {
       if (isBreak == false) {
         displayModeText.innerText = 'Hit Start to Take A Break';
@@ -54,27 +54,30 @@ startStopBtn.addEventListener('click', function() {
         displayModeText.style.color = 'red';
     }
     return;
-  } //else if (isActive === false) {
+  }
     currentIntervalId = setInterval(decrement, 1000);
     resetTime.disabled = true;
     isActive = true;
     startStopBtn.innerText = 'stop';
     workBtn.disabled = true;
     breakBtn.disabled = true;
+    if (min == 25) {
+    min--;
+    } else if (min == 5 && isBreak == true) {
+      min--;
+    }
     if (min == 0 && sec ==0){
       resetTime.disabled = true;
     }
     if (min == 0 && sec == 0) {
       if (isBreak == false) {
         isBreak = true; //sets to work time
-        min = 0;
-        sec = 6;
+        breakTimeDisplay();
         //displayModeText.innerText = 'Hit Start to Take A Break';
         //startStopBtn.disabled = false;
       } else if (isBreak == true) {
         isBreak = !isBreak; //sets to break time
-        min = 0;
-        sec = 5;
+        workTimeDisplay();
         //startStopBtn.disabled = false;
       }
     }
@@ -87,22 +90,18 @@ resetTime.addEventListener('click', function(e) {
   if (min == 0 && sec == 0) {
     if (isBreak == true) {
       isBreak = !isBreak; //sets to work time
-      min = 0;
-      sec = 6;
+      breakTimeDisplay();
       //startStopBtn.disabled = false;
     } else if (isBreak == false) {
       isBreak = true; //sets to break time
-      min = 0;
-      sec = 5;
+      workTimeDisplay();
       //startStopBtn.disabled = false;
     }
   }  if (min != 0 || sec != 0) {
         if (isBreak == true) {
-          min = 0;
-          sec = 6;
+          breakTimeDisplay();
         } else if (isBreak == false){
-          min = 0;
-          sec = 5;
+          workTimeDisplay();
         }
     }  
   console.log(isBreak);
@@ -111,6 +110,7 @@ resetTime.addEventListener('click', function(e) {
 //MAIN TIMER FUNCTION
 function decrement() {
   sec--;
+  currentMinEl.innerText = min;
   currentSecEl.innerText = sec;
   if (sec < 10){
     currentSecEl.innerText = '0' + sec;
@@ -126,7 +126,23 @@ function decrement() {
   }
   if (sec == 0 && min == 0) {
     clearTimeout(currentIntervalId);    
+    audioBell.play();
     //startStopBtn.disabled = true;
     //resetTime.disabled = false;
   }
 }
+
+function workTimeDisplay() {
+  min = 25;
+  sec = 60;
+  currentMinEl.innerText = min;
+  currentSecEl.innerText = defaultNum;
+}
+
+function breakTimeDisplay() {
+  currentMinEl.innerText = '0' + 5;
+  currentSecEl.innerText = defaultNum;
+  min = 5;
+  sec = 60;
+}
+
